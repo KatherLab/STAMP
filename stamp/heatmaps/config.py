723af384f3c0a3532fab4343a46a5981b7875538
Marko van Treeck <markovantreeck@gmail.com>
2024-10-08 14:37:43 +0100

    Update default config

--- stamp/heatmaps/config.py
+++ stamp/heatmaps/config.py
@@ -6,6 +6,8 @@ from pydantic import AliasChoices, BaseModel, Field
 class HeatmapConfig(BaseModel):
     feature_dir: Path
     wsi_dir: Path
-    checkpoint_path: Path = Field(alias=AliasChoices("checkpoint_path", "model_path"))
+    checkpoint_path: Path = Field(
+        validation_alias=AliasChoices("checkpoint_path", "model_path")
+    )
     output_dir: Path
     overview: bool = True
