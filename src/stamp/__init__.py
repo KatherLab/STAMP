86d239bdce53053d388d8504c848f433431e3295
Marko van Treeck <markovantreeck@gmail.com>
2024-12-16 15:11:49 +0000

    Update dependencies

--- src/stamp/__init__.py
+++ src/stamp/__init__.py
@@ -1,3 +1,9 @@
 from importlib.metadata import version
 
+from beartype import BeartypeConf
+from beartype.claw import beartype_this_package
+
+# Warn about all incorrect type annotations
+beartype_this_package(conf=BeartypeConf(violation_type=UserWarning))
+
 __version__: str = version("stamp")
