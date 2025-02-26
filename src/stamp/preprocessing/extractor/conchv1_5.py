451c9376633c6ae5beae71ff32a3f5783910bedf
Juan Pablo <juanpabloricapito@gmail.com>
2025-02-26 13:33:02 +0000

    add multiple extractors

---
+++ src/stamp/preprocessing/extractor/conchv1_5.py
@@ -0,0 +1,20 @@
+try:
+    from transformers import AutoModel 
+except ModuleNotFoundError as e:
+    raise ModuleNotFoundError(
+        "conchv1_5 dependencies not installed."
+        " Please reinstall stamp using `pip install 'stamp[conchv1_5]'`"
+    ) from e
+
+from stamp.preprocessing.extractor import Extractor
+
+
+def conchv1_5():
+    """Extracts features from slide tiles using GigaPath tile encoder."""
+    titan = AutoModel.from_pretrained('MahmoodLab/TITAN', trust_remote_code=True)
+    model, transform = titan.return_conch()
+    return Extractor(
+        model=model,
+        transform=transform,
+        identifier="conchv1_5",  # type: ignore
+    )
---
+++ src/stamp/preprocessing/extractor/h_optimus_1.py
@@ -0,0 +1,30 @@
+try:
+    import timm 
+    from torchvision import transforms
+except ModuleNotFoundError as e:
+    raise ModuleNotFoundError(
+        "h_optimus_1 dependencies not installed."
+        " Please reinstall stamp using `pip install 'stamp[h_optimus_1]'`"
+    ) from e
+
+from stamp.preprocessing.extractor import Extractor
+
+
+def h_optimus_1():
+    model = timm.create_model(
+        "hf-hub:bioptimus/H-optimus-1", pretrained=True, init_values=1e-5,dynamic_img_size=False
+    )
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=(0.707223, 0.578729, 0.703617), 
+            std=(0.211883, 0.230117, 0.177517)
+        ),
+    ])
+
+    return Extractor(
+        model=model,
+        transform=transform,
+        identifier="h_optimus_1",  # type: ignore
+    )
