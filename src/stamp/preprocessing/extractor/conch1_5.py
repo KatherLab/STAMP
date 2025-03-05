d5264c5a7efde8c0e008b8946f5c30574c6ae544
Juan Pablo <juanpabloricapito@gmail.com>
2025-03-05 14:47:13 +0000

    fix conch1_5

--- src/stamp/preprocessing/extractor/conch1_5.py
+++ src/stamp/preprocessing/extractor/conch1_5.py
@@ -2,11 +2,7 @@ from collections.abc import Callable
 from typing import cast
 
 try:
-    import timm
-    from PIL.Image import Image
-    from timm.data.config import resolve_data_config
-    from timm.data.transforms_factory import create_transform
-    from torch import Tensor
+    from transformers import AutoModel
 except ModuleNotFoundError as e:
     raise ModuleNotFoundError(
         "conchv1_5 dependencies not installed."
@@ -17,9 +13,8 @@ from stamp.preprocessing.extractor import Extractor
 
 
 def conch1_5() -> Extractor:
-    model = timm.create_model("hf_hub:MahmoodLab/conchv1_5", pretrained=True)
-    transform = cast(
-        Callable[[Image], Tensor],
-        create_transform(**resolve_data_config(model.pretrained_cfg, model=model)),
+    titan = AutoModel.from_pretrained("MahmoodLab/TITAN", trust_remote_code=True)
+    model, eval_transform = titan.return_conch()
+    return Extractor(
+        model=model, transform=eval_transform, identifier="mahmood-conch1_5"
     )
-    return Extractor(model=model, transform=transform, identifier="mahmood-conch1_5")
