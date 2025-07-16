e798090e0600128ab6570612b4a9f81e20b70891
Juan Pablo <juanpabloricapito@gmail.com>
2025-07-16 16:28:14 +0100

    add prism

--- src/stamp/encoding/__init__.py
+++ src/stamp/encoding/__init__.py
@@ -64,6 +64,11 @@ def init_slide_encoder_(
 
             selected_encoder: Encoder = Madeleine()
 
+        case EncoderName.PRISM:
+            from stamp.encoding.encoder.prism import Prism
+
+            selected_encoder: Encoder = Prism()
+
         case Encoder():
             selected_encoder = encoder
 
