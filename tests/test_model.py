7361a6a792d3e79094386d6357581040594a9507
mducducd <minhducddd@gmail.com>
2025-09-02 09:14:11 +0100

    fix test model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,5 @@
 import torch
 
-from stamp.modeling.classifier import LitPatientlassifier
 from stamp.modeling.classifier.mlp import MLPClassifier
 from stamp.modeling.classifier.vision_tranformer import VisionTransformer
 
@@ -80,17 +79,13 @@ def test_mlp_classifier_dims(
     dim_hidden: int = 64,
     num_layers: int = 2,
 ) -> None:
-    model = LitPatientlassifier(
+    model = MLPClassifier(
         categories=[str(i) for i in range(num_classes)],
         category_weights=torch.ones(num_classes),
         dim_input=input_dim,
-        model=MLPClassifier(
-            input_dim,
-            dim_hidden,
-            dim_output=num_classes,
-            num_layers=num_layers,
-            dropout=0.1,
-        ),
+        dim_hidden=dim_hidden,
+        num_layers=num_layers,
+        dropout=0.1,
         ground_truth_label="test",
         train_patients=["pat1", "pat2"],
         valid_patients=["pat3", "pat4"],
@@ -111,17 +106,13 @@ def test_mlp_inference_reproducibility(
     dim_hidden: int = 64,
     num_layers: int = 3,
 ) -> None:
-    model = LitPatientlassifier(
+    model = MLPClassifier(
         categories=[str(i) for i in range(num_classes)],
         category_weights=torch.ones(num_classes),
         dim_input=input_dim,
-        model=MLPClassifier(
-            input_dim,
-            dim_hidden,
-            num_classes,
-            num_layers,
-            0.1,
-        ),
+        dim_hidden=dim_hidden,
+        num_layers=num_layers,
+        dropout=0.1,
         ground_truth_label="test",
         train_patients=["pat1", "pat2"],
         valid_patients=["pat3", "pat4"],
