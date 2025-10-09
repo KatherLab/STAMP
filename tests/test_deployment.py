30053a88afe8737b5116416adbc1affabb76ddcb
mducducd <minhducddd@gmail.com>
2025-10-09 15:19:14 +0100

    add tests

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -131,10 +131,16 @@ def test_predict_patient_level(
         predictions[patient_ids[0]], more_predictions[patient_ids[0]]
     ), "the same inputs should repeatedly yield the same results"
 
-
-def test_to_prediction_df() -> None:
+@pytest.mark.parametrize("task", ["classification", "regression", "survival"])
+def test_to_prediction_df(task: str) -> None:
+    if task == "classification":
+        ModelClass = LitTileClassifier
+    elif task == "regression":
+        ModelClass = LitTileRegressor
+    else:
+        ModelClass = LitTileSurvival
     n_heads = 7
-    model = LitTileClassifier(
+    model = ModelClass(
         model_class=VisionTransformer,
         categories=["foo", "bar", "baz"],
         category_weights=torch.tensor([0.1, 0.2, 0.7]),
@@ -145,6 +151,8 @@ def test_to_prediction_df() -> None:
         n_layers=2,
         dropout=0.5,
         ground_truth_label="test",
+        time_label="time",
+        status_label="status",
         train_patients=np.array(["pat1", "pat2"]),
         valid_patients=np.array(["pat3", "pat4"]),
         use_alibi=False,
@@ -152,45 +160,80 @@ def test_to_prediction_df() -> None:
         max_lr=1e-4,
         div_factor=25,
     )
+    if task == "classification":
+        preds_df = _to_prediction_df(
+            categories=list(model.categories),  # type: ignore
+            patient_to_ground_truth={
+                PatientId("pat5"): GroundTruth("foo"),
+                PatientId("pat6"): None,
+                PatientId("pat7"): GroundTruth("baz"),
+            },
+            patient_label="patient",
+            ground_truth_label="target",
+            predictions={
+                PatientId("pat5"): torch.rand((3)),
+                PatientId("pat6"): torch.rand((3)),
+                PatientId("pat7"): torch.rand((3)),
+            },
+        )
 
-    preds_df = _to_prediction_df(
-        categories=list(model.categories),
-        patient_to_ground_truth={
-            PatientId("pat5"): GroundTruth("foo"),
-            PatientId("pat6"): None,
-            PatientId("pat7"): GroundTruth("baz"),
-        },
-        patient_label="patient",
-        ground_truth_label="target",
-        predictions={
-            PatientId("pat5"): torch.rand((3)),
-            PatientId("pat6"): torch.rand((3)),
-            PatientId("pat7"): torch.rand((3)),
-        },
-    )
+        # Check if all expected columns are included
+        assert {
+            "patient",
+            "target",
+            "pred",
+            "target_foo",
+            "target_bar",
+            "target_baz",
+            "loss",
+        } <= set(preds_df.columns)
+        assert len(preds_df) == 3
 
-    # Check if all expected columns are included
-    assert {
-        "patient",
-        "target",
-        "pred",
-        "target_foo",
-        "target_bar",
-        "target_baz",
-        "loss",
-    } <= set(preds_df.columns)
-    assert len(preds_df) == 3
+        # Check if no loss / target is given for targets with missing ground truths
+        no_ground_truth = preds_df[preds_df["patient"].isin(["pat6"])]
+        assert no_ground_truth["target"].isna().all()  # pyright: ignore[reportGeneralTypeIssues,reportAttributeAccessIssue]
+        assert no_ground_truth["loss"].isna().all()  # pyright: ignore[reportGeneralTypeIssues,reportAttributeAccessIssue]
 
-    # Check if no loss / target is given for targets with missing ground truths
-    no_ground_truth = preds_df[preds_df["patient"].isin(["pat6"])]
-    assert no_ground_truth["target"].isna().all()  # pyright: ignore[reportGeneralTypeIssues,reportAttributeAccessIssue]
-    assert no_ground_truth["loss"].isna().all()  # pyright: ignore[reportGeneralTypeIssues,reportAttributeAccessIssue]
+        # Check if loss / target is given for targets with ground truths
+        with_ground_truth = preds_df[preds_df["patient"].isin(["pat5", "pat7"])]
+        assert (~with_ground_truth["target"].isna()).all()  # pyright: ignore[reportGeneralTypeIssues,reportAttributeAccessIssue]
+        assert (~with_ground_truth["loss"].isna()).all()  # pyright: ignore[reportGeneralTypeIssues,reportAttributeAccessIssue]
 
-    # Check if loss / target is given for targets with ground truths
-    with_ground_truth = preds_df[preds_df["patient"].isin(["pat5", "pat7"])]
-    assert (~with_ground_truth["target"].isna()).all()  # pyright: ignore[reportGeneralTypeIssues,reportAttributeAccessIssue]
-    assert (~with_ground_truth["loss"].isna()).all()  # pyright: ignore[reportGeneralTypeIssues,reportAttributeAccessIssue]
+    elif task == "regression":
+        patient_to_ground_truth = {}
+        predictions = {PatientId(f"pat{i}"): torch.randn(1) for i in range(5)}
+        categories = []
+        preds_df = _to_regression_prediction_df(
+            patient_to_ground_truth=patient_to_ground_truth,
+            patient_label="patient",
+            ground_truth_label="target",
+            predictions=predictions,
+        )
+        assert "patient" in preds_df.columns
+        assert "pred" in preds_df.columns
+        assert len(preds_df) > 0
 
+        assert "loss" in preds_df.columns
+        assert preds_df["loss"].isna().all()
+    else:
+        patient_to_ground_truth = {
+            PatientId("p1"): "10.0 1",
+            PatientId("p2"): "12.3 0",
+        }
+        predictions = {
+            PatientId("p1"): torch.tensor([0.8]),
+            PatientId("p2"): torch.tensor([0.2]),
+        }
+        categories = []
+        preds_df = _to_survival_prediction_df(
+            patient_to_ground_truth=patient_to_ground_truth,
+            patient_label="patient",
+            ground_truth_label="target",
+            predictions=predictions,
+        )
+        assert "patient" in preds_df.columns
+        assert "pred_risk" in preds_df.columns
+        assert len(preds_df) > 0
 
 @pytest.mark.filterwarnings("ignore:GPU available but not used")
 @pytest.mark.filterwarnings(
