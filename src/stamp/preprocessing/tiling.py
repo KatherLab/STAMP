094b9d0e12d49cd542ea9a072ede18e172bf4d45
mducducd <minhducddd@gmail.com>
2025-11-20 09:39:24 +0000

    update supertile_size_um

--- src/stamp/preprocessing/tiling.py
+++ src/stamp/preprocessing/tiling.py
@@ -314,7 +314,7 @@ def _supertiles(
     )
     supertile_size_tile_px = TilePixels(tile_size_px * len_of_supertile_in_tiles)
 
-    supertile_size_um = Microns(tile_size_um * len_of_supertile_in_tiles)
+    supertile_size_um = Microns(supertile_size_slide_px * slide_mpp)
 
     with futures.ThreadPoolExecutor(max_workers) as executor:
         futs = []
